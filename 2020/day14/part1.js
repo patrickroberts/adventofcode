81cf851b01392355afd9ea6954b71e753b7e4831
Patrick Roberts <pat2060@sbcglobal.net>
2020-12-14 00:59:27 -0600

    Solved day 14

---
+++ 2020/day14/part1.js
@@ -0,0 +1,19 @@
+import input from '../../input.js';
+import accumulate from '../accumulate.js';
+
+let set, clr;
+const mem = new Map();
+
+for (const [, dst, addr, val] of input.matchAll(/^(\w+)\[?(\d*)\]? = (\w+)$/gm)) {
+  switch (dst) {
+    case 'mask':
+      set = BigInt(parseInt(val.replace(/X/g, 0), 2));
+      clr = BigInt(parseInt(val.replace(/X/g, 1), 2));
+      break;
+    case 'mem':
+      mem.set(addr, Number((BigInt(val) | set) & clr));
+      break;
+  }
+}
+
+console.log(accumulate(Array.from(mem.values()), 0));
---
+++ 2020/day14/part2.js
@@ -0,0 +1,36 @@
+import input from '../../input.js';
+import accumulate from '../accumulate.js';
+
+let mask;
+const mem = new Map();
+
+for (const [, dst, addr, val] of input.matchAll(/^(\w+)\[?(\d*)\]? = (\w+)$/gm)) {
+  switch (dst) {
+    case 'mask':
+      mask = val;
+      break;
+    case 'mem':
+      apply(BigInt(addr), Number(val));
+      break;
+  }
+}
+
+console.log(accumulate(Array.from(mem.values()), 0));
+
+function apply(address, value) {
+  let width = 0;
+  const result = mask.replace(/./g, (c, i) => {
+    switch (c) {
+      case '0': return (address >> BigInt(35 - i)) & 1n;
+      case 'X': ++width;
+      case '1': return c;
+    }
+  });
+  const max = 1 << width;
+
+  for (let bits = 0; bits < max; ++bits) {
+    let bit = width;
+    const actual = result.replace(/X/g, () => (bits >> --bit) & 1);
+    mem.set(parseInt(actual, 2), value);
+  }
+}
